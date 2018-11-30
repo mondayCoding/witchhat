70d06def3c3b5525789d91eb099c097d411c2375
mondayCoding <moriluxuria@gmail.com>
2018-11-30 02:40:42 +0200

    demo commit

--- src/Layout/Site.tsx
+++ src/Layout/Site.tsx
@@ -7,9 +7,6 @@ import navicons from '../UtilsUI/Icons';
 import { NavLink, BrowserRouter } from 'react-router-dom';
 
 const App = () => {
-	const [count, setCount] = useState(0);
-	useEffect(logMousePosition, []);
-
 	return (
 		<div className="App">
 			<header className="App-header">
@@ -25,26 +22,51 @@ const App = () => {
 				>
 					Learn React
 				</a>
-				<h2>{count}</h2>
-				<button onClick={() => setCount(count + 1)}>increase</button>
-				<button onClick={() => setCount(count - 1)}>decrease</button>
 				<Main />
 			</header>
 		</div>
 	);
 };
 
-const linkProps = {
-	exact: true,
-	activeClassName: 'active',
-	className: 'nav--item'
+const COUNTER = () => {
+	const [count, setCount] = useState(0);
+
+	return (
+		<React.Fragment>
+			<h2>{count}</h2>
+			<button onClick={() => setCount(count + 1)}>increase</button>
+			<button onClick={() => setCount(count - 1)}>decrease</button>
+		</React.Fragment>
+	);
 };
+
+const LOGPOSITION = () => {
+	useEffect(logMousePosition, []);
+
+	return <h4>Logging mouse position to console</h4>;
+};
+
+const logMousePosition = () => {
+	console.log('listener created');
+	window.addEventListener('mousemove', logmousepos);
+
+	return () => {
+		console.log('listener removed');
+		window.removeEventListener('mousemove', logmousepos);
+	};
+};
+
+const logmousepos = (e: any) => console.log({ x: e.clientX, y: e.clientY });
+
+const HOMEROUTE = () => <h2>HOME</h2>;
+const ANOTHERROUTE = () => <h2>ANOTHER</h2>;
+const THIRDROUTE = () => <h2>THIRD</h2>;
 
 const Main = () => (
 	<BrowserRouter>
 		<div className="layout">
 			<nav className="nav">
-				<NavLink to={routes.SIGN_IN} {...linkProps}>
+				{/* <NavLink to={routes.SIGN_IN} {...linkProps}>
 					{navicons.dashboard} Sign In
 				</NavLink>
 
@@ -66,16 +88,26 @@ const Main = () => (
 
 				<NavLink to={routes.PASSWORD_FORGET} {...linkProps}>
 					{navicons.missions} Forgot password
+				</NavLink> */}
+
+				<NavLink to={'/counter'} {...linkProps}>
+					{navicons.missions} Counter
+				</NavLink>
+
+				<NavLink to={'/mouse_logger'} {...linkProps}>
+					{navicons.missions} Mouse Logger
 				</NavLink>
 			</nav>
 			<main className="main">
 				<Switch>
-					<Route exact={true} path={routes.SIGN_IN} component={HOMEROUTE} />
+					{/* <Route exact={true} path={routes.SIGN_IN} component={HOMEROUTE} />
 					<Route exact={true} path={routes.SIGN_UP} component={ANOTHERROUTE} />
 					<Route exact={true} path={routes.LANDING} component={THIRDROUTE} />
 					<Route exact={true} path={routes.HOME} component={HOMEROUTE} />
 					<Route exact={true} path={routes.ACCOUNT} component={ANOTHERROUTE} />
-					<Route exact={true} path={routes.PASSWORD_FORGET} component={THIRDROUTE} />
+					<Route exact={true} path={routes.PASSWORD_FORGET} component={THIRDROUTE} /> */}
+					<Route exact={true} path={'/counter'} component={COUNTER} />
+					<Route exact={true} path={'/mouse_logger'} component={LOGPOSITION} />
 					<Redirect to="/" />
 				</Switch>
 			</main>
@@ -83,19 +115,10 @@ const Main = () => (
 	</BrowserRouter>
 );
 
-const HOMEROUTE = () => <h2>HOME</h2>;
-const ANOTHERROUTE = () => <h2>ANOTHER</h2>;
-const THIRDROUTE = () => <h2>THIRD</h2>;
-
-const logMousePosition = () => {
-	console.log('create');
-	window.addEventListener('mousemove', logmousepos);
-	return () => {
-		console.log('cleaning');
-		window.removeEventListener('mousemove', logmousepos);
-	};
+const linkProps = {
+	exact: true,
+	activeClassName: 'active',
+	className: 'nav--item'
 };
-
-const logmousepos = (e: any) => console.log({ x: e.clientX, y: e.clientY });
 
 export default App;
